a78740c4617ef802e8e5356d451af47682703e38
dannyfadli <danyfdly95@gmail.com>
2024-10-08 10:26:18 +0700

    tugas pyhton

---
+++ main.py
@@ -0,0 +1,24 @@
+#mencetak nama saya
+print("Ardhian fadli Rabbani")
+print()
+
+#mencetak i am learning pyhton basic
+print("i am learning pyhton basic")
+print()
+
+#mencetak nama yang sudah disimpan dalam variabel
+Name = "Ardhian Fadli Rabbani"
+print("Hallo", Name)
+print()
+
+#pada line 1 deklarasi variabel nama dan di print,pada line menambahkan sapaan berupa hallo dan memanggil variabel nama
+
+"""
+Program ini bertujuan untuk meminta input umur pengguna dan kemudian mencetak umur tersebut.
+Program ini menggunakan fungsi input() untuk meminta input dari pengguna dan print sendiri berfungsi untuk mencetak hasilnya.
+"""
+
+age = input("Berapa umur Anda? ")
+print(age)
+
+
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+void Insertionsort(){
+	int temp;
+	int n;
